6722f01d12af785e76e00bfbaea3d8f8f7edcdc1
Steven Hunal <stevehunal@gmail.com>
2019-08-01 02:39:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+require "pry"
+class School
+  
+def initialize(roster)
+  roster = {}
+  @roster = roster
+end
+
+def roster
+  @roster
+end 
+  
+def add_student(name, grade)
+  
+  if roster.include [grade]
+    [grade] << name
+  else
+    roster[grade] = []
+  roster[grade] << name
+end
+end
+
+end 
+school = School.new("Bayside High School")
+
+#if roster has the grade in question push the student name else create the array for roster grade and push the student
