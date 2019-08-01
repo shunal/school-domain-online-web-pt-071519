5e46535b7056b9207012ff0b01ed7517442aeb90
Steven Hunal <stevehunal@gmail.com>
2019-08-01 02:44:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,13 +11,12 @@ def roster
 end 
   
 def add_student(name, grade)
-  
-  if roster.include grade
+  if roster.include? grade
     roster[grade] << name
   else
     roster[grade] = []
   roster[grade] << name
-end
+  end
 end
 
 
